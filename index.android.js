c1c6282391451270ddf851c8a3d95bb56830199a
Nilton Vasques <nilton.vasques@openmailbox.org>
2017-01-30 10:37:50 -0300

    Built initial screen

--- index.android.js
+++ index.android.js
@@ -9,23 +9,48 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Button,
+  Alert
 } from 'react-native';
+
+class Logo extends Component {
+  render() {
+    let pic = {
+      uri: 'http://www.secnet.me/images/secnet/social-icons/support.png'
+    };
+    return (
+        <Image source={pic} style={{width: 200, height: 200}}/>
+    );
+  }
+}
+const onButtonPress = () => { Alert.alert('Button has been pressed!'); };
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
 export default class TicketSystem extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.logo}>
+          <Logo style={styles.logoImage} />
+        </View>
+
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to CrossOver Ticket System!
         </Text>
+
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          To get started, sign in in the application, or create an account pressing sign up button.
         </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <Button onPress={onButtonPress} title="Sign in" accessibilityLabel="Sign in into ticket system" />
+        <Button color="lightgreen" onPress={onButtonPress} title="Sign up" accessibilityLabel="Sign up into ticket system" />
       </View>
     );
   }
@@ -34,6 +59,11 @@ export default class TicketSystem extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -47,6 +77,19 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    justifyContent: 'center',
+  },
+  signIn: {
+    flex: 1,
+  },
+  signUp: {
+    color: 'steelblue',
+    backgroundColor: '#F5FCFF',
+    tintColor: '#F5FCFF',
+  },
+  logoImage: {
+    textAlign: 'center',
+    alignItems: 'center',
   },
 });
 
