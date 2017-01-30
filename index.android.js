7666f057bbf84f620b0e31776d5c1de19ca332ec
Nilton Vasques <nilton.vasques@openmailbox.org>
2017-01-30 20:12:43 -0300

    Setup apollo client

--- index.android.js
+++ index.android.js
@@ -4,35 +4,42 @@
  * @flow
  */
 
+// React
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Navigator
-} from 'react-native';
+import { AppRegistry, Navigator } from 'react-native';
 
+// Apollo
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+// Scenes
 import MainScene from './MainScene';
 import SigninScene from './SigninScene';
 import SignupScene from './SignupScene';
 import RequestsScene from './RequestsScene';
 
+const client = new ApolloClient();
+
 export default class TicketSystem extends Component {
   render() {
     return (
-      <Navigator 
-        initialRoute={{ screen: 'MainScene', index: 0 }}
-        renderScene={(route, nav) => {
-          switch(route.screen) {
-            case "MainScene":
-              return <MainScene navigator={nav} />
-            case "SigninScene":
-              return <SigninScene navigator={nav} />
-            case "SignupScene":
-              return <SignupScene navigator={nav} />
-            case "RequestsScene":
-              return <RequestsScene navigator={nav} />
-          }
-        }}
-      />
+        <ApolloProvider client={client}>
+          <Navigator 
+            initialRoute={{ screen: 'MainScene', index: 0 }}
+            renderScene={(route, nav) => {
+              switch(route.screen) {
+                case "MainScene":
+                  return <MainScene navigator={nav} />
+                case "SigninScene":
+                    return <SigninScene navigator={nav} />
+                case "SignupScene":
+                      return <SignupScene navigator={nav} />
+                case "RequestsScene":
+                        return <RequestsScene navigator={nav} />
+              }
+            }}
+          />
+        </ApolloProvider>
     );
   }
 }
